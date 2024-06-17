249ee53bf66ce3da2c0dec1d6e79c21d2aa75eb5
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-06-17 13:55:42 +0200

    User authentication

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from account import views
 
 urlpatterns = [
@@ -20,5 +22,9 @@ urlpatterns = [
     
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
