b1638cbc344e3a75bfa20ae0b98cbbbf40495516
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-06-24 17:55:48 +0200

    Making a new images app

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'social_django',
     'account.apps.AccountConfig',
     'django.contrib.admin',
@@ -169,6 +170,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
