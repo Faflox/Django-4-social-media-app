38e10061ab5ce7ef66a1f1eb1fc0b1cd0d9a8be2
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-07-19 08:21:17 +0200

    g

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,21 +5,6 @@ from django.conf.urls.static import static
 from account import views
 
 urlpatterns = [
-    #login/logout
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
-    
-    #zmiana hasła
-    #path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    #path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    #odzyskiwanie hasła
-    #path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    #path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    #path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
